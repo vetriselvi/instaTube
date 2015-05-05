ff2a49e9f782606094da411930763d9bd9aea398
Vetri Selvi Vairamuthu <vairamut@usc.edu>
2015-05-05 12:57:34 -0700

    Update instatube.js
    Updated the comments and removed the test alert codes.

--- assets/js/instatube.js
+++ assets/js/instatube.js
@@ -4,37 +4,17 @@ $(document).ready(function()
 //MACRO for number of results
 var resultsVarTotal = 9;
 
-//To compensate for the ':/' video
+//To compensate for the devicesupport video
 var resultsVar = resultsVarTotal + 1;
  //var searchBox = $("#search_input);
 $("#search_input").focus();
 $("#search_input").keyup(function() 
 {
- /*
- //function search() {
-  var q = $('#search_input').val();
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    part: 'snippet'
-  });
 
-  request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    $('#video').html('<pre>' + str + '</pre>');
-  });
-//} -->
-*/
  $("#video").html('');
 var search_input = $(this).val();
 var keyword= encodeURIComponent(search_input);
- 
 var yt_url='http://gdata.youtube.com/feeds/api/videos?q='+keyword+'&format=5&max-results='+resultsVar+'&v=2&alt=jsonc'; 
-
-//http://suggestqueries.google.com/complete/search?q=[QUERY]&output=youtube
-//var yt_url='https://www.youtube.com/results?search_query='+keyword+'&format=5&max-results=6&v=2&alt=jsonc'; 
-//var yt_url='http://gdata.youtube.com/feeds/api/videos?q='+encodeURIComponent(searchBox.val())+'&format=5&max-results=6&v=2&alt=jsonc'; 
-//https://www.youtube.com/results?search_query= 
- 
 $.ajax({
 type: "GET",
 url: yt_url,
@@ -43,17 +23,12 @@ success: function(response)
 {
 if(response.data.items)
 {
- 
- 
- 
 $.each(response.data.items, function(i,data)
 {
-
 var video_id=data.id;
 var video_title=data.title;
 var video_viewCount=data.viewCount;
- 
- //To avoid the ':/' faced videos
+//To avoid the device support  videos
  if(video_id!='UKY3scPIMd8'){
  //iframe - youtube videos
 var video_frame="<iframe width='340' height='250' src='http://www.youtube.com/embed/"+video_id+"' frameborder='0' type='text/html'></iframe>";
