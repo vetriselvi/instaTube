fe29659d68e9ce885bb402ee3e4f58599f5e80bf
Vetri Selvi Vairamuthu <vairamut@usc.edu>
2015-05-08 13:36:28 -0700

    Update instatube.js
    Migrated to Youtube Data API version 3

--- assets/js/instatube.js
+++ assets/js/instatube.js
@@ -2,48 +2,49 @@
 $(document).ready(function()
 {
 //MACRO for number of results
-var resultsVarTotal = 9;
+var resultsVarTotal = 20;
 
-//To compensate for the devicesupport video
-var resultsVar = resultsVarTotal + 1;
- //var searchBox = $("#search_input);
-$("#search_input").focus();
-$("#search_input").keyup(function() 
-{
+//To compensate for any un-supported form of videos in future
+var resultsVar = resultsVarTotal ;
+ 
+$("#search_input").keyup(function(){
 
  $("#video").html('');
 var search_input = $(this).val();
+
 var keyword= encodeURIComponent(search_input);
-var yt_url='http://gdata.youtube.com/feeds/api/videos?q='+keyword+'&format=5&max-results='+resultsVar+'&v=2&alt=jsonc'; 
+
+//Request url - [Youtube Data API-> version 3-> Reference-> Search:List]
+var yt_url='https://www.googleapis.com/youtube/v3/search?part=snippet&q='+keyword+'&maxResults='+resultsVar+'&key=AIzaSyDMV-vxpsJDQ-hb8B5nQLC54VUS8n4kYpM';
+
+
 $.ajax({
 type: "GET",
 url: yt_url,
 dataType:"jsonp",
 success: function(response)
 {
-if(response.data.items)
+
+if(response.items){
+
+$.each( response.items, function(i,data)
 {
-$.each(response.data.items, function(i,data)
-{
-var video_id=data.id;
-var video_title=data.title;
-var video_viewCount=data.viewCount;
-//To avoid the device support  videos
- if(video_id!='UKY3scPIMd8'){
- //iframe - youtube videos
-var video_frame="<iframe width='340' height='250' src='http://www.youtube.com/embed/"+video_id+"' frameborder='0' type='text/html'></iframe>";
-var final="<div id='result'  ><div>"+video_frame+"</div></div>";
- }
-$("#video").append(final);
-});
-}
+var video_id=data.id.videoId;
+var video_frame="<iframe width='255' height='150' src='http://www.youtube.com/embed/"+video_id+"' frameborder='0' type='text/html'></iframe>";
+var final="<div><div id='result' class='result'>"+video_frame+"</div></div>";
+$("#video").append(final); 
+}); //.each
+} //response.data.item
 else
 {
 	$("#video").html("<div id='no'>No Video</div>");
 }
+
 } 
 });
-});
+ 
+}); //keyup
+
 });
  
 
